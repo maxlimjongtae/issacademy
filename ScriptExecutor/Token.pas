1ac006008c9c033ac600b9a44db403d5edc037bc
Kelly <0xae80@gmail.com>
2020-08-01 23:41:54 +0900

    Move Identifier into Token

--- ScriptExecutor/Token.pas
+++ ScriptExecutor/Token.pas
@@ -3,14 +3,31 @@ unit Token;
 interface
 
 uses
-  Identifier;
+ SysUtils, System.Rtti;
 
 type
+  TTokenType = (Declaration, Variable, WhiteSpace, TypeDefinition, TypeValue,
+    Definition, Value, Concat, EndOfStatement, OpenBracket, CloseBracket,
+    StringQuote, StringType, IntegerType, FunctionType);
+
+  TIdentifier = class
+  private
+    FValue: string;
+    FTokenType: TTokenType;
+  public
+    constructor Create(Value: string; TokenType: TTokenType);
+
+    property V: string read FValue;
+    property T: TTokenType read FTokenType;
+  end;
+
   TPosition = class
   private
     FRow, FCol: Integer;
   public
     constructor Create(Row, Col: Integer);
+
+    function ToString: string; override;
 
     property R: Integer read FRow;
     property C: Integer read FCol;
@@ -22,15 +39,27 @@ type
     FIdentifier: TIdentifier;
 
     function GetIdentifierValue: string;
+    function GetTokenType: TTokenType;
   public
     constructor Create(Position: TPosition; Identifier: TIdentifier);
     destructor Destroy; override;
 
+    function ToString: string; override;
+
     property Position: TPosition read FPosition;
     property V: string read GetIdentifierValue;
+    property T: TTokenType read GetTokenType;
   end;
 
 implementation
+
+{ TIdentifier }
+
+constructor TIdentifier.Create(Value: string; TokenType: TTokenType);
+begin
+  FValue := Value;
+  FTokenType := TokenType;
+end;
 
 { TPosition }
 
@@ -40,11 +69,26 @@ begin
   FCol := Col;
 end;
 
+function TPosition.ToString: string;
+begin
+  Result := 'Line: '+ IntToStr(R) + ', Position: ' + IntToStr(C);
+end;
+
 { TToken }
 
-function TToken.GetIdentifierValue;
+function TToken.GetIdentifierValue: string;
 begin
   Result := FIdentifier.V;
+end;
+
+function TToken.ToString: string;
+begin
+  Result := 'Value: ' + V + ', Type: ' + TRttiEnumerationType.GetName<TTokenType>(T) + '\n' +Position.ToString;
+end;
+
+function TToken.GetTokenType: TTokenType;
+begin
+  Result := FIdentifier.T;
 end;
 
 constructor TToken.Create(Position: TPosition; Identifier: TIdentifier);
